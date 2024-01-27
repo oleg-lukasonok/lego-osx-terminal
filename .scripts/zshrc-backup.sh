0dc9661c1d07febf955854a7d1ea4af667e821e1
lego <oleg.lukasonok@lt.ibm.com>
2024-01-27 22:15:19 +0200

    ...

--- .scripts/zshrc-backup.sh
+++ .scripts/zshrc-backup.sh
@@ -10,4 +10,12 @@ _echoInfoSeparator
 _echoInfo "# Backing ~/.zshrc into ${LEGO_REPO_HOME_DIR}/.zshrc/zshrc"
 _echoInfo "# "
 _echoInfo "# "
+rm -rf ${LEGO_REPO_HOME_DIR}/.zshrc/zshrc/utils
+rm -rf ${LEGO_REPO_HOME_DIR}/.zshrc/zshrc
+
 cp ~/.zshrc ${LEGO_REPO_HOME_DIR}/.zshrc/zshrc
+cp ~/.zshrc/utils ${LEGO_REPO_HOME_DIR}/.zshrc/utils
+
+_echoInfoSeparator
+cat "${LEGO_REPO_HOME_DIR}/.zshrc/zshrc"
+_echoInfoSeparator
--- .scripts/zshrc-restore.sh
+++ .scripts/zshrc-restore.sh
@@ -10,11 +10,17 @@ _echoInfoSeparator
 _echoInfo "# Restoring ${LEGO_REPO_HOME_DIR}/.zshrc/zshrc into ~/.zshrc"
 _echoInfo "# "
 _echoInfo "# "
+rm -rf ~/.zshrc-utils
+rm -rf ~/.zshrc
+
 cp ${LEGO_REPO_HOME_DIR}/.zshrc/zshrc ~/.zshrc
+cp ${LEGO_REPO_HOME_DIR}/.zshrc/zshrc/utils ~/.zshrc-utils
 
 _echoInfoSeparator
 
 cat ~/.zshrc
+
+ls -la ~/.zshrc
 
 _echoInfoSeparator
 _echoInfo "# Reloading ~/.zshrc file"
