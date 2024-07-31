c6bf1fca957486aa83ec0ec0d9aa5ac6bba26a7b
lego <oleg.lukasonok@gmail.com>
2024-07-31 21:57:47 +0300

    ...

--- .zshrc/utils/-utils-go-2.sh
+++ .zshrc/utils/-utils-go-2.sh
@@ -4,19 +4,29 @@
 #
 
 go2() {
-  TMP_ARGS=("$@")
-
-  TMP_PROJECT=""
+  local TMP_ARGS=("$@")
+  local TMP_PROJECT=""
   while [[ "$#" -gt 0 ]]; do
     case $1 in
-    --project)
+    --projects)
       export TMP_PROJECT="$2"
+      cd "${HOME}/projects/${TMP_PROJECT}"
+      ;;
+    --projects-ibm)
+      export TMP_PROJECT_IBM="$2"
+      cd "${HOME}/projects-ibm/${TMP_PROJECT}"
+      ;;
+    --projects-roche)
+      export TMP_PROJECT_IBM="$2"
+      cd "${HOME}/projects-roche/${TMP_PROJECT}"
+      ;;
+    --projects-personal)
+      export TMP_PROJECT_IBM="$2"
+      cd "${HOME}/projects-personal/${TMP_PROJECT}"
       ;;
     *) ;;
     esac
     shift
   done
   set -- "${TMP_ARGS[@]}"
-
-  cd "${HOME}/projects/${TMP_PROJECT}"
 }
